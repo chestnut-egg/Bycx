19ae4d449d7319ab6f7fa704dab945ce6d586577
dan <1048232111@qq.com>
2018-04-21 16:49:09 +0800

    First Django

---
+++ HelloWorld/HelloWorld/view.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    context          = {}
+    context['hello'] = 'Hello World!!'
+    context['hi'] = 'HI!'
+    return render(request, 'student_home.html', context)
